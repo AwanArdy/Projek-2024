202bd376a6f4fb2f6cd51906d059c1f66c28b094
Muhammad Awan Ardy Firmansyah <muhammadawan46@gmail.com>
2024-01-06 06:56:39 +0800

    revisi lagi njir wkwkwk

--- Building-a-Date-Formatter/script.js
+++ Building-a-Date-Formatter/script.js
@@ -12,14 +12,14 @@ const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
 dateOptionsSelectElement.addEventListener("change", () => {
-  switch (dateOptionsSelectElement) {
+  switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
       currentDateParagraph.textContent = formattedDate
         .split("-")
         .reverse()
         .join("-");
       break;
-    case "mm-dd-yyy-h-mm":
+    case "mm-dd-yyyy-h-mm":
       currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
       break;
     default:

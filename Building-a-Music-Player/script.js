8ec745f448fb68d45ca25aa692719520f86bf3c9
Muhammad Awan Ardy Firmansyah <muhammadawan46@gmail.com>
2024-01-06 05:53:04 +0800

    selesai

--- Building-a-Music-Player/script.js
+++ Building-a-Music-Player/script.js
@@ -8,73 +8,73 @@ const shuffleButton = document.getElementById("shuffle");
 const allSongs = [
   {
     id: 0,
-    title: "Hito wa Yume wo Nido Miru",
-    artist: "Nogizaka46",
-    duration: "5:43",
-    src: "https://open.spotify.com/track/3W1KL5dUISmtye75QhNohl?si=c10ce6bfacb4466e",
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
   },
   {
     id: 1,
-    title: "Tanin no Sora ni",
-    artist: "Nogizaka46",
-    duration: "4:43",
-    src: "https://open.spotify.com/track/7fDXaxje3LekPDGpLmt9WX?si=c50f3b25cb7f441f",
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
   },
   {
     id: 2,
-    title: "Garasu Mado ga Yogorateru",
-    artist: "Hinatazaka46",
-    duration: "4:00",
-    src: "https://open.spotify.com/track/6NW0OhKaFymA6fqVjThApx?si=348bdb20fa574b00",
+    title: "Still Learning",
+    artist: "Quincy Larson",
+    duration: "3:51",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
   },
   {
     id: 3,
-    title: "I'm in",
-    artist: "Sakurazaka46",
-    duration: "5:01",
-    src: "https://open.spotify.com/track/2Y51hgeUpf5DgH4X8NgJUn?si=4806c03759b34731",
+    title: "Cruising for a Musing",
+    artist: "Quincy Larson",
+    duration: "3:34",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
   },
   {
     id: 4,
-    title: "One Choice",
-    artist: "Hinatazaka46",
-    duration: "4:44",
-    src: "https://open.spotify.com/track/5t8ERaOJ3p2vnMKWTBflcy?si=3c27af215cb7411f",
+    title: "Never Not Favored",
+    artist: "Quincy Larson",
+    duration: "3:35",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
   },
   {
     id: 5,
-    title: "Ohitorisama Tengoku",
-    artist: "Nogizaka46",
-    duration: "4:15",
-    src: "https://open.spotify.com/track/22e9NP0TRCFhOGsgBnl6Ua?si=f0755bb1b1cf4e4a",
+    title: "From the Ground Up",
+    artist: "Quincy Larson",
+    duration: "3:12",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
   },
   {
     id: 6,
-    title: "Sakurazuki",
-    artist: "Sakurazaka46",
-    duration: "4:43",
-    src: "https://open.spotify.com/track/2jPyj1FZgRaE5xjugKqmvC?si=7cbf3eb045754bf8",
+    title: "Walking on Air",
+    artist: "Quincy Larson",
+    duration: "3:25",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
   },
   {
     id: 7,
-    title: "Natsu no Chikamichi",
-    artist: "Sakurazaka46",
-    duration: "5:09",
-    src: "https://open.spotify.com/track/5Ucv9VBRDeBy514OEdcic6?si=971086ff9b254923",
+    title: "Can't Stop Me. Can't Even Slow Me Down.",
+    artist: "Quincy Larson",
+    duration: "3:52",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
   },
   {
     id: 8,
-    title: "Akubi Letter",
-    artist: "Hinatazaka46",
-    duration: "4:12",
-    src: "https://open.spotify.com/track/2Uh1RsG87xjNbgQHw9R7un?si=01819a16ea1d4720",
+    title: "The Surest Way Out is Through",
+    artist: "Quincy Larson",
+    duration: "3:10",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
   },
   {
     id: 9,
-    title: "Tteka",
-    artist: "Hinatazaka46",
-    duration: "3:58",
-    src: "https://open.spotify.com/track/3qhatIxwcJTvTRvKcr9boJ?si=b610c557452a4a34",
+    title: "Chasing That Feeling",
+    artist: "Quincy Larson",
+    duration: "2:43",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
   },
 ];
 
@@ -132,6 +132,50 @@ const playPreviousSong = () => {
   }
 };
 
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
+const deleteSong = (id) => {
+  if (userData?.currentSong?.id === id) {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+
+    pauseSong();
+    setPlayerDisplay();
+  }
+
+  userData.songs = userData?.songs.filter((song) => song.id !== id);
+  renderSongs(userData?.songs);
+  highlightCurrentSong();
+  setPlayButtonAccessibleText();
+
+  if (userData.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset Playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+
+      renderSongs(userData?.songs);
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+  }
+};
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
@@ -165,7 +209,7 @@ const renderSongs = (array) => {
             <span class="playlist-song-artist">${song.artist}</span>
             <span class="playlist-song-duration">${song.duration}</span>
         </button>
-        <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+        <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
             <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
         </button>
@@ -201,5 +245,24 @@ pauseButton.addEventListener("click", pauseSong);
 nextButton.addEventListener("click", playNextSong);
 
 previousButton.addEventListener("click", playPreviousSong);
+
+shuffleButton.addEventListener("click", shuffle);
+
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] != undefined;
+
+  if (nextSongExists) {
+    playNextSong();
+  } else {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+
+    pauseSong();
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+  }
+});
 
 renderSongs(userData?.songs);
